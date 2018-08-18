e94d2877ceb23925fa895e60a297340c69e6065b
josephyangzx <zyan0155@uni.sydney.edu.au>
2018-08-18 19:28:53 +1000

    Coming soon
    Coming soon

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 14, 2019 18:03:26",
+	date: "October 22, 2018 23:59:99",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
